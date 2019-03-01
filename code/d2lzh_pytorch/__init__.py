d8d80a63c49c00b7a88d90d9c87e8e0b11de7212
ShusenTang <tangshusen@pku.edu.cn>
2019-03-01 21:16:20 +0800

    add 3.2 and d2lzh_pytorch

---
+++ code/d2lzh_pytorch/__init__.py
@@ -0,0 +1,2 @@
+from .utils import *
+
---
+++ code/d2lzh_pytorch/utils.py
@@ -0,0 +1,60 @@
+import collections
+import math
+import os
+import random
+import sys
+import tarfile
+import time
+import zipfile
+
+from IPython import display
+from matplotlib import pyplot as plt
+# import mxnet as mx
+# from mxnet import autograd, gluon, image, init, nd
+# from mxnet.contrib import text
+# from mxnet.gluon import data as gdata, loss as gloss, nn, utils as gutils
+import numpy as np
+
+
+VOC_CLASSES = ['background', 'aeroplane', 'bicycle', 'bird', 'boat',
+               'bottle', 'bus', 'car', 'cat', 'chair', 'cow',
+               'diningtable', 'dog', 'horse', 'motorbike', 'person',
+               'potted plant', 'sheep', 'sofa', 'train', 'tv/monitor']
+
+
+VOC_COLORMAP = [[0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0],
+                [0, 0, 128], [128, 0, 128], [0, 128, 128], [128, 128, 128],
+                [64, 0, 0], [192, 0, 0], [64, 128, 0], [192, 128, 0],
+                [64, 0, 128], [192, 0, 128], [64, 128, 128], [192, 128, 128],
+                [0, 64, 0], [128, 64, 0], [0, 192, 0], [128, 192, 0],
+                [0, 64, 128]]
+
+
+
+# ###################### 3.2 ############################
+def set_figsize(figsize=(3.5, 2.5)):
+    use_svg_display()
+    # 设置图的尺寸
+    plt.rcParams['figure.figsize'] = figsize
+
+def use_svg_display():
+    """Use svg format to display plot in jupyter"""
+    display.set_matplotlib_formats('svg')
+
+def data_iter(batch_size, features, labels):
+    num_examples = len(features)
+    indices = list(range(num_examples))
+    random.shuffle(indices)  # 样本的读取顺序是随机的
+    for i in range(0, num_examples, batch_size):
+        j = torch.LongTensor(indices[i: min(i + batch_size, num_examples)]) # 最后一次可能不足一个batch
+        yield  features.index_select(0, j), labels.index_select(0, j) 
+
+def linreg(X, w, b):
+    return torch.mm(X, w) + b
+
+def squared_loss(y_hat, y): 
+    return (y_hat - y.view(y_hat.size())) ** 2 / 2
+
+def sgd(params, lr, batch_size):
+    for param in params:
+        param.data -= lr * param.grad / batch_size # 注意这里更改param时用的param.data
