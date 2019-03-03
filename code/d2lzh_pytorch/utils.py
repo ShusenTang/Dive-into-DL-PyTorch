bb9b550813a3fed148946c332806bd347e61a610
ShusenTang <tangshusen@pku.edu.cn>
2019-03-03 21:13:54 +0800

    add 3.5

--- code/d2lzh_pytorch/utils.py
+++ code/d2lzh_pytorch/utils.py
@@ -9,6 +9,8 @@ import zipfile
 
 from IPython import display
 from matplotlib import pyplot as plt
+import torchvision
+import torchvision.transforms as transforms
 # import mxnet as mx
 # from mxnet import autograd, gluon, image, init, nd
 # from mxnet.contrib import text
@@ -58,3 +60,33 @@ def squared_loss(y_hat, y):
 def sgd(params, lr, batch_size):
     for param in params:
         param.data -= lr * param.grad / batch_size # 注意这里更改param时用的param.data
+
+
+
+# ######################3##### 3.5 #############################
+def get_fashion_mnist_labels(labels):
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+def show_fashion_mnist(images, labels):
+    use_svg_display()
+    # 这里的_表示我们忽略（不使用）的变量
+    _, figs = plt.subplots(1, len(images), figsize=(12, 12))
+    for f, img, lbl in zip(figs, images, labels):
+        f.imshow(img.view((28, 28)).numpy())
+        f.set_title(lbl)
+        f.axes.get_xaxis().set_visible(False)
+        f.axes.get_yaxis().set_visible(False)
+    plt.show()
+
+def load_data_fashion_mnist(batch_size, root='~/Datasets/FashionMNIST'):
+    """Download the fashion mnist dataset and then load into memory."""
+    transform = transforms.ToTensor()
+    mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, download=True, transform=transform)
+    mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, download=True, transform=transform)
+
+    train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=4)
+    test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=4)
+
+    return train_iter, test_iter
