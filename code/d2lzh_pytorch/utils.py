15f5a0ccb4a72a36dbe6933f9ff324fbd23aaa57
ShusenTang <tangshusen@pku.edu.cn>
2019-03-18 22:11:55 +0800

    add 5,5

--- code/d2lzh_pytorch/utils.py
+++ code/d2lzh_pytorch/utils.py
@@ -163,21 +163,22 @@ def semilogy(x_vals, y_vals, x_label, y_label, x2_vals=None, y2_vals=None,
 
 
 # ############################# 3.13 ##############################
-def evaluate_accuracy(data_iter, net):
-    acc_sum, n = 0.0, 0
-    for X, y in data_iter:
-        if isinstance(net, torch.nn.Module):
-            net.eval() # 评估模式, 这会关闭dropout
-            acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
-            net.train() # 改回训练模式
-        else: # 自定义的模型
-            if('is_training' in net.__code__.co_varnames): # 如果有is_training这个参数
-                # 将is_training设置成False
-                acc_sum += (net(X, is_training=False).argmax(dim=1) == y).float().sum().item() 
-            else:
-                acc_sum += (net(X).argmax(dim=1) == y).float().sum().item() 
-        n += y.shape[0]
-    return acc_sum / n
+# 5.5 修改
+# def evaluate_accuracy(data_iter, net):
+#     acc_sum, n = 0.0, 0
+#     for X, y in data_iter:
+#         if isinstance(net, torch.nn.Module):
+#             net.eval() # 评估模式, 这会关闭dropout
+#             acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
+#             net.train() # 改回训练模式
+#         else: # 自定义的模型
+#             if('is_training' in net.__code__.co_varnames): # 如果有is_training这个参数
+#                 # 将is_training设置成False
+#                 acc_sum += (net(X, is_training=False).argmax(dim=1) == y).float().sum().item() 
+#             else:
+#                 acc_sum += (net(X).argmax(dim=1) == y).float().sum().item() 
+#         n += y.shape[0]
+#     return acc_sum / n
 
 
 
@@ -192,3 +193,46 @@ def corr2d(X, K):  # 本函数已保存在d2lzh_pytorch包中方便以后使用
         for j in range(Y.shape[1]):
             Y[i, j] = (X[i: i + h, j: j + w] * K).sum()
     return Y
+
+
+
+# ############################ 5.5 #########################
+def evaluate_accuracy(data_iter, net):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    acc_sum, n = 0.0, 0
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(net, torch.nn.Module):
+                net.eval() # 评估模式, 这会关闭dropout
+                acc_sum += (net(X.to(device)).argmax(dim=1) == y.to(device)).float().sum().cpu().item()
+                net.train() # 改回训练模式
+            else: # 自定义的模型, 3.13节之后不会用到, 不考虑GPU
+                if('is_training' in net.__code__.co_varnames): # 如果有is_training这个参数
+                    # 将is_training设置成False
+                    acc_sum += (net(X, is_training=False).argmax(dim=1) == y).float().sum().item() 
+                else:
+                    acc_sum += (net(X).argmax(dim=1) == y).float().sum().item() 
+            n += y.shape[0]
+    return acc_sum / n
+
+
+def train_ch5(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs):
+    net = net.to(device)
+    print("training on ", device)
+    loss = torch.nn.CrossEntropyLoss()
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n, start = 0.0, 0.0, 0, time.time()
+        for X, y in train_iter:
+            X = X.to(device)
+            y = y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y).sum()
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+            train_l_sum += l.cpu().item()
+            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().cpu().item()
+            n += y.shape[0]
+        test_acc = evaluate_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, time %.1f sec'
+              % (epoch + 1, train_l_sum / n, train_acc_sum / n, test_acc, time.time() - start))
