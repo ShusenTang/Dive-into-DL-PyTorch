d935be7755adf7787f760dba5cbf8fb58959423a
ShusenTang <tangshusen@pku.edu.cn>
2019-03-26 23:24:57 +0800

    update 3.5, set num_works=0 if Win

--- code/d2lzh_pytorch/utils.py
+++ code/d2lzh_pytorch/utils.py
@@ -91,9 +91,12 @@ def show_fashion_mnist(images, labels):
 #     transform = transforms.ToTensor()
 #     mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, download=True, transform=transform)
 #     mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, download=True, transform=transform)
-
-#     train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=4)
-#     test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=4)
+#     if sys.platform.startswith('win'):
+#         num_workers = 0  # 0表示不用额外的进程来加速读取数据
+#     else:
+#         num_workers = 4
+#     train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+#     test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
 #     return train_iter, test_iter
 
@@ -252,9 +255,12 @@ def load_data_fashion_mnist(batch_size, resize=None, root='~/Datasets/FashionMNI
     transform = torchvision.transforms.Compose(trans)
     mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, download=True, transform=transform)
     mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, download=True, transform=transform)
-
-    train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=4)
-    test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=4)
+    if sys.platform.startswith('win'):
+        num_workers = 0  # 0表示不用额外的进程来加速读取数据
+    else:
+        num_workers = 4
+    train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
     return train_iter, test_iter
 
