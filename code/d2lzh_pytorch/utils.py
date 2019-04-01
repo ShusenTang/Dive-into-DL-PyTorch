810db3fcd1b6d91b616de43088cf20397072f002
ShusenTang <tangshusen@pku.edu.cn>
2019-04-01 21:47:47 +0800

    add 6.3

--- code/d2lzh_pytorch/utils.py
+++ code/d2lzh_pytorch/utils.py
@@ -295,3 +295,57 @@ class Residual(nn.Module):
         if self.conv3:
             X = self.conv3(X)
         return F.relu(Y + X)
+
+
+
+
+# ############################## 6.3 ##################################3
+def load_data_jay_lyrics():
+    """加载周杰伦歌词数据集"""
+    with zipfile.ZipFile('../../data/jaychou_lyrics.txt.zip') as zin:
+        with zin.open('jaychou_lyrics.txt') as f:
+            corpus_chars = f.read().decode('utf-8')
+    corpus_chars = corpus_chars.replace('\n', ' ').replace('\r', ' ')
+    corpus_chars = corpus_chars[0:10000]
+    idx_to_char = list(set(corpus_chars))
+    char_to_idx = dict([(char, i) for i, char in enumerate(idx_to_char)])
+    vocab_size = len(char_to_idx)
+    corpus_indices = [char_to_idx[char] for char in corpus_chars]
+    return corpus_indices, char_to_idx, idx_to_char, vocab_size
+
+# 本函数已保存在d2lzh_pytorch包中方便以后使用
+def data_iter_random(corpus_indices, batch_size, num_steps, device=None):
+    # 减1是因为输出的索引x是相应输入的索引y加1
+    num_examples = (len(corpus_indices) - 1) // num_steps
+    epoch_size = num_examples // batch_size
+    example_indices = list(range(num_examples))
+    random.shuffle(example_indices)
+
+    # 返回从pos开始的长为num_steps的序列
+    def _data(pos):
+        return corpus_indices[pos: pos + num_steps]
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    for i in range(epoch_size):
+        # 每次读取batch_size个随机样本
+        i = i * batch_size
+        batch_indices = example_indices[i: i + batch_size]
+        X = [_data(j * num_steps) for j in batch_indices]
+        Y = [_data(j * num_steps + 1) for j in batch_indices]
+        yield torch.tensor(X, dtype=torch.float32, device=device), torch.tensor(Y, dtype=torch.float32, device=device)
+
+# 本函数已保存在d2lzh_pytorch包中方便以后使用
+def data_iter_consecutive(corpus_indices, batch_size, num_steps, device=None):
+    if device is None:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    corpus_indices = torch.tensor(corpus_indices, dtype=torch.float32, device=device)
+    data_len = len(corpus_indices)
+    batch_len = data_len // batch_size
+    indices = corpus_indices[0: batch_size*batch_len].view(batch_size, batch_len)
+    epoch_size = (batch_len - 1) // num_steps
+    for i in range(epoch_size):
+        i = i * num_steps
+        X = indices[:, i: i + num_steps]
+        Y = indices[:, i + 1: i + num_steps + 1]
+        yield X, Y
