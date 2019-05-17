e13d9d64f8322fafda8a0cbb90830630400a56f7
ShusenTang <tangshusen@pku.edu.cn>
2019-05-17 23:59:11 +0800

    add 9.1

--- code/d2lzh_pytorch/utils.py
+++ code/d2lzh_pytorch/utils.py
@@ -648,3 +648,18 @@ class Benchmark():
 
     def __exit__(self, *args):
         print('%stime: %.4f sec' % (self.prefix, time.time() - self.start))
+
+
+
+
+
+# ########################### 9.1 ########################################
+def show_images(imgs, num_rows, num_cols, scale=2):
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = d2l.plt.subplots(num_rows, num_cols, figsize=figsize)
+    for i in range(num_rows):
+        for j in range(num_cols):
+            axes[i][j].imshow(imgs[i * num_cols + j])
+            axes[i][j].axes.get_xaxis().set_visible(False)
+            axes[i][j].axes.get_yaxis().set_visible(False)
+    return axes
