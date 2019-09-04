4f0cdd8af30160750e8d7ba40ec9eba1ab126333
ShusenTang <tangshusen@pku.edu.cn>
2019-09-04 17:19:15 +0800

    fix bug in bbox_to_rect

--- code/d2lzh_pytorch/utils.py
+++ code/d2lzh_pytorch/utils.py
@@ -719,7 +719,7 @@ def train(train_iter, test_iter, net, loss, optimizer, device, num_epochs):
 def bbox_to_rect(bbox, color):
     # 将边界框(左上x, 左上y, 右下x, 右下y)格式转换成matplotlib格式：
     # ((左上x, 左上y), 宽, 高)
-    return d2l.plt.Rectangle(
+    return plt.Rectangle(
         xy=(bbox[0], bbox[1]), width=bbox[2]-bbox[0], height=bbox[3]-bbox[1],
         fill=False, edgecolor=color, linewidth=2)
 
