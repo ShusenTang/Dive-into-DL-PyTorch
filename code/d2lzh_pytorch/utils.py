27a5109542f5973e627cd3118327104aade23a88
ShusenTang <tangshusen@pku.edu.cn>
2019-10-29 18:39:16 +0800

    fix bug in MultiBoxPrior

--- code/d2lzh_pytorch/utils.py
+++ code/d2lzh_pytorch/utils.py
@@ -777,7 +777,7 @@ def show_bboxes(axes, bboxes, labels=None, colors=None):
     colors = _make_list(colors, ['b', 'g', 'r', 'm', 'c'])
     for i, bbox in enumerate(bboxes):
         color = colors[i % len(colors)]
-        rect = d2l.bbox_to_rect(bbox.detach().cpu().numpy(), color)
+        rect = bbox_to_rect(bbox.detach().cpu().numpy(), color)
         axes.add_patch(rect)
         if labels and len(labels) > i:
             text_color = 'k' if color == 'w' else 'w'
